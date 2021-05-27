1ea1cfa583f0d27ccce9d021022f9050178ce47c
Vishal <dvishal485@gmail.com>
2021-05-27 19:11:22 +0530

    Basic files added

---
+++ header.js
@@ -0,0 +1,16 @@
+const header = (req_headers) => {
+    return {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Max-Age": "86400",
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+        "Your-IP": req_headers.get("cf-connecting-ip"),
+        "Your-Country": req_headers.get("CF-IPCountry"),
+        "Host": req_headers.get("host"),
+        "Made-By": atob('VmlzaGFsIERhcyBodHRwczovL2dpdGh1Yi5jb20vZHZpc2hhbDQ4NQ==')
+    }
+}
+
+export default header
---
+++ index.js
@@ -0,0 +1,27 @@
+//import getUpdates from './getUpdates'
+import header from './header'
+
+async function handleRequest(request) {
+    const headers = header(request.headers)
+    const path = new URL(request.url).pathname;
+
+    if (request.method == 'GET') {
+        if (path.startsWith('/search')) {
+            return new Response('{"info":"search"}', {
+                status: 200,
+                headers
+            })
+        } else {
+            return new Response('{"repo":"https://github.com/dvishal485/flipkart-scraper/"}', {
+                status: 200,
+                headers
+            })}
+    } else {
+        return Response.redirect("https://github.com/dvishal485/flipkart-scraper/", 301)
+    }
+   
+}
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
