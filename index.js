717319504d9961dd947481e87703f3b519330dc1
Vishal Das <dvishal485@gmail.com>
2022-03-31 21:41:33 +0530

    all major changes will now come under v3.0.0

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function handleRequest(request) {
                 {
                     "name": "flipkart-scraper",
                     "description": "API to scrapes search result and product details from Flipkart",
-                    "version": "2.1.0",
+                    "version": "3.0.0",
                     "author": "Vishal Das",
                     "email": "dvishal485@gmail.com",
                     "documentation": "https://dvishal485.github.io/flipkart-scraper-api/",
