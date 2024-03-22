2900b9c7dc34497093f0d923f04c3f77a6022583
Vishal <dvishal485@gmail.com>
2024-03-22 15:23:33 +0530

    update backend
    Signed-off-by: Vishal <dvishal485@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -104,9 +104,6 @@ async fn main() {
         }));
 
     println!("Starting server on port 3000");
-
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
